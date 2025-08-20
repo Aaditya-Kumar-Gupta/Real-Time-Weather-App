4841e395a52e0c01d2a1fbe83f2ac8fb4afac7f6
Cody-aditya <adityakumar6043@gmail.com>
2025-08-21 01:38:52 +0530

    Update 1.5

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        | Download{" "}
+        <a href="https://github.com/ISHDEEP-CHANDIGARHUNIVERSITY/test-a1-Cody-aditya">
+          Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="https://github.com/Cody-aditya">
+          Aditya Kumar Gupta
+        </a>{" "}
+        | Student of{" "}
+        <a target="_blank" href="https://www.cuchd.in/">
+          Chandigarh University |
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "ad68353c7f82f4cadda646e33dba943c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
